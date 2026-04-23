ac5b0d70f87e3835837fa432752f0caf4b99b8f5
komalshinde16 <komalambadasshinde@gmail.com>
2026-04-23 17:46:20 +0530

    demo3

---
+++ src/main/java/com/itshaala/OBJECT_CLASS/Demo3.java
@@ -0,0 +1,12 @@
+package com.itshaala.OBJECT_CLASS;
+
+
+
+public class Demo3 {
+    public static void main(String[] args) {
+        Demo3 obj = new Demo3();
+        System.out.println(obj.getClass().getName());
+//        Demo2LeapYear obj2 = new Demo2LeapYear();
+//        System.out.println(obj2.getClass().getName());
+    }
+}
