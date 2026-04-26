c6b93f8962e53476f147bbd80bb4a775eb29a779
komalshinde16 <komalambadasshinde@gmail.com>
2026-04-26 10:17:51 +0530

    9

---
+++ src/main/java/com/itshaala/OBJECT_CLASS/Demo9ShallowCopy.java
@@ -0,0 +1,62 @@
+package com.itshaala.OBJECT_CLASS;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+public class Demo9ShallowCopy {
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        @AllArgsConstructor
+        @NoArgsConstructor
+        @Data
+        @ToString
+        class Address {
+            private String city;
+            private String state;
+            private String country;
+        }
+
+        @AllArgsConstructor
+        @NoArgsConstructor
+        @Data
+        @ToString
+        class Employee implements Cloneable{
+            private int id;
+            private String name;
+            private double salary;
+            private Address address;
+
+            @Override
+            protected Object clone() throws CloneNotSupportedException {
+                return super.clone();
+            }
+        }
+
+        Employee employee1 = new Employee();
+
+        employee1.setId(1);
+        employee1.setName("John");
+        employee1.setSalary(50000);
+
+        Address address = new Address();
+        address.setCity("San Francisco");
+        address.setState("CA");
+        address.setCountry("US");
+
+        employee1.setAddress(address);
+
+        Employee employee2 = (Employee) employee1.clone();
+
+        System.out.println(employee1);
+        System.out.println(employee2);
+        System.out.println("address change");
+        employee1.getAddress().setCity("Pune");
+        System.out.println(employee1);
+        System.out.println(employee2);
+
+
+    }
+
+}
